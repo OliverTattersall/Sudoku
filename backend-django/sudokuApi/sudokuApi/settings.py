f0a7bf44b19805c451bc505de56f475a243e8322
oliver tattersall <oliver.m.tattersall@gmail.com>
2023-07-04 18:04:15 -0400

    changed database

--- backend-django/sudokuApi/sudokuApi/settings.py
+++ backend-django/sudokuApi/sudokuApi/settings.py
@@ -80,22 +80,26 @@ WSGI_APPLICATION = 'sudokuApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'sudokuAPI',
-       'USER': os.getenv('localpsqluser'),
-       'PASSWORD': os.getenv('localpsqlpassword'),
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'sudokuApi',
+    'USER': os.getenv('psqluser'),
+    'PASSWORD': os.getenv('psqlpassword'),
+    'HOST': 'ep-holy-sun-679523.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+  }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'sudokuAPI',
+#        'USER': os.getenv('localpsqluser'),
+#        'PASSWORD': os.getenv('localpsqlpassword'),
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
